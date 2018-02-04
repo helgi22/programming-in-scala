547fbe3da7c8476e6bed8e291f76fb6b28cbfa9d
Oleg.Galaykovskiy <helgi.arney@gmail.com>
2018-02-04 22:58:31 +0200

    ++ renewal chapter 16 methods of List()

--- src/main/scala/chapter16_WorkWithLists/MsortMethod.scala
+++ src/main/scala/chapter16_WorkWithLists/MsortMethod.scala
@@ -7,7 +7,11 @@ object MsortMethod {
     def merge(xs: List[T], ys: List[T]): List[T] = (xs, ys) match {
       case (Nil, _) => ys
       case (_, Nil) => xs
-      case (x :: xs1, y :: ys1) => if (less(x, y)) x :: merge(xs1, ys) else y :: merge(xs, ys1)
+      case (x :: xs1, y :: ys1) =>
+        if (less(x, y))
+          x :: merge(xs1, ys)
+        else
+          y :: merge(xs, ys1)
     }
 
     val n = xs.length / 2
@@ -20,7 +24,10 @@ object MsortMethod {
 
   def main(args: Array[String]): Unit = {
 
-    val rez = msort((x: Int, y: Int) => x < y)(List(1, 3, 2, 7, 5, 6))
-    println(s"Result is $rez")
+    val inSort = msort((x: Int, y: Int) => x < y) _
+    val reverseInSort = msort((x: Int, y: Int) => x > y) _
+    val rez = inSort(List(1, 3, 2, 7, 5, 6))
+    val reverseRes = reverseInSort(List(1, 3, 2, 7, 5, 6))
+    println(s"Result is $rez and reverse $reverseRes")
   }
 }
